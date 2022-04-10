0811101bdcfb083dda99044d3aba1b78edbda9e4
DLinh <nogamenolife.why@gmail.com>
2022-04-10 15:26:45 +0700

    style: folder structure

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
+import {About, Footer, Header, Skills, Testimonial, Work} from './container'
+import { Navbar } from './components' 
+import './App.scss'
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
   )
 }
 
---
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Navbar.scss'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
