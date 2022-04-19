f09db09f4624335d4fb43f6ef349a6ef6acbb6ce
DLinh <nogamenolife.why@gmail.com>
2022-04-19 20:34:08 +0700

    feat: not testimonial

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const NavigationDots = ({ active }) => {
+  // 'testimonials'
   return (
     <div className='app__navigation'>
-      {['home', 'about',  'work', 'skills', 'testimonials', 'contact'].map(
+      {['home', 'about',  'work', 'skills', 'contact'].map(
         (item, idx) => (
-          <a key={item + idx} href={`#${item}`} className="app__navigation-dot" style={active === item ? {backgroundColor: '#313BAC'} : {}}/>
+          <a key={item + idx} href={`#${item}`} className="app__navigation-dot" style={active === item ? {backgroundColor: '#313BAC'} : {}}>
+            {''}
+          </a>
         )
       )}
     </div>
