6757c0bc67d93e6d5ee4dab6db9ab79ba8f4f75a
DLinh <nogamenolife.why@gmail.com>
2022-06-28 13:52:56 +0700

    update package

--- frontend_react/src/index.jsx
+++ frontend_react/src/index.jsx
@@ -4,5 +4,4 @@ import App from "./App";
 import './index.css';
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-root.render(<App />);
+createRoot(rootElement).render(<App />);
